d82bff54007e70fcd693085a9b5a0e2eef2d6a7b
Adityo <muhammadadityogunawan@gmail.com>
2022-09-13 16:43:12 +0700

    test push

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -21,5 +22,11 @@ func main() {
 	factory.InitFact(e, db)
 
 	log.Fatal(e.Start(fmt.Sprintf(":%s", config.DB_SERVERPORT)))
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE},
+	}))
 
 }
